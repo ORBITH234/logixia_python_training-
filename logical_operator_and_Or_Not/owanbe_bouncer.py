d0dbb712ca65c19d1579ab16e9cf261119fd2123
idakpan <Oladosuolamide330@gmail.com>
2026-07-23 11:24:16 +0100

    owanbe_added

---
+++ logical_operator_and_Or_Not/owanbe_bouncer.py
@@ -0,0 +1,9 @@
+bouncer_rule = 0
+has_iv = True
+aso_ebi_wears = False
+input("do you have invit(y/n)? ")
+input("do you wear aso ebi (y/n)? ")
+if bouncer_rule == has_iv and aso_ebi_wears:
+    print("please enter and have your sit")
+else:
+    print("you are not welcome")
